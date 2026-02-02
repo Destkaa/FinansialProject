be10b0e692c5ba8dab7b06d5acca1da56ea1e6af
Kaka Viangi <kakaviangi58@gmail.com>
2026-02-02 10:52:31 +0700

    Table

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('saldo', App\Http\Controllers\SaldoController::class);
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
